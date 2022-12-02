176029a4b70e0934cda1cb52b4a66695b0c86da4
LittleLebowski <o.f.atas@gmail.com>
2022-12-02 15:30:36 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import * as Blabla from "../data/user.js";
 
 function App() {
+  const { username, city, image } = Blabla;
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city} />
+      <About image={image} />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function Home() {
+function Home(props) {
+  const { username, city } = props;
   return (
     <div id="home">
       <h1>
@@ -9,3 +10,4 @@ function Home() {
     </div>
   );
 }
+export default Home;
